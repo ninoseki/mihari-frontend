06d977e133dcdf1cc155ae7018e950951b77f673
Manabu Niseki <manabu.niseki@gmail.com>
2022-10-10 11:29:56 +0900

    chore: use vite instead of vue-cli

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
   transform: {
     "^.+\\.vue$": "@vue/vue3-jest",
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
   },
 };
