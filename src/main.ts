9b761f84bf8dd9cf4977949facf971098fd9843f
Manabu Niseki <manabu.niseki@gmail.com>
2023-04-22 18:48:27 +0900

    chore: update deps

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import "bulma/css/bulma.css";
 import "bulma-helpers/css/bulma-helpers.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
-// Polyfill
-import "regenerator-runtime/runtime";
 
 import { createApp } from "vue";
 
-import App from "./App.vue";
-import router from "./router";
+import App from "@/App.vue";
+import router from "@/router";
 
 createApp(App).use(router).mount("#app");
